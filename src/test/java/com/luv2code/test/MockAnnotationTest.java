3b188ce7b08091cf38b229d9c51c4b8f6333911e
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-07-30 11:27:18 -0300

    create MockAnnotationTest class and set up a few properties

---
+++ src/test/java/com/luv2code/test/MockAnnotationTest.java
@@ -0,0 +1,41 @@
+package com.luv2code.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import com.luv2code.component.MvcTestingExampleApplication;
+import com.luv2code.component.dao.ApplicationDao;
+import com.luv2code.component.models.CollegeStudent;
+import com.luv2code.component.models.StudentGrades;
+import com.luv2code.component.service.ApplicationService;
+
+@SpringBootTest(classes = MvcTestingExampleApplication.class)
+public class MockAnnotationTest {
+
+	@Autowired
+	ApplicationContext context;
+
+	@Autowired
+	CollegeStudent student;
+
+	@Autowired
+	StudentGrades studentGrades;
+
+	@Mock
+	private ApplicationDao applicationDao;
+
+	@InjectMocks
+	private ApplicationService applicationService;
+
+	@BeforeEach
+	void beforeEach() {
+		this.student.setFirstname("Mano");
+		this.student.setLastname("Dahora");
+		this.student.setEmailAddress("mano@mail.com");
+		this.student.setStudentGrades(this.studentGrades);
+	}
+}
